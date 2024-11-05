61219f93129c45e5aeb42f5ddc50ef074ba24452
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-11-05 13:07:19 +0300

    Delete randomA.py

--- randomA.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import random
-import sys
-
-try:
-    A = random.randint(-10, 10)
-    print(A)
-except Exception as e:
-    print(f"Ошибка в программе randomA: {e}", file=sys.stderr)

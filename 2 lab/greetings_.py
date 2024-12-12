4c19695ee2bc7ba6beb0176f4195c39847452770
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-13 00:01:45 +0300

    Update greetings_.py

--- 2 lab/greetings_.py
+++ 2 lab/greetings_.py
@@ -2,57 +2,37 @@
 import re
 import sys
 
-
-# Функция проверки имени
 def is_valid_name(name):
     return bool(re.match(r"^[A-Z][a-zA-Z]*$", name))
 
-
-def greet(name):
-    print(f"Привет, {name}!")
-
-
-# Режим 1:
 def greet_from_file(filename):
     try:
-        with open(filename, "r") as file:
-            names = file.readlines()
-
-        with open("error.txt", "w") as error_file:
-            for name in names:
-                name = name.strip()
+        with open(filename, "r") as file, open("error.txt", "w") as error_file:
+            for line in file:
+                name = line.strip()
                 if is_valid_name(name):
-                    greet(name)
+                    print(f"Привет, {name}!")
                 else:
-                    error_file.write(f"Ошибка: Некорректное имя '{name}'\n")
+                    error_file.write(f"Некорректное имя: {name}\n")
     except FileNotFoundError:
-        print(f"Ошибка: Файл '{filename}' не найден.", file=sys.stderr)
-    except Exception as e:
-        print(f"Произошла ошибка: {e}", file=sys.stderr)
+        print(f"Файл '{filename}' не найден.", file=sys.stderr)
 
-
-# Режим 2
 def greet_interactive():
     try:
         while True:
             name = input("Введите имя: ").strip()
             if is_valid_name(name):
-                greet(name)
+                print(f"Привет, {name}!")
             else:
-                print("Ошибка: Некорректное имя. Имя должно начинаться с заглавной буквы и содержать только буквы.")
+                print("Некорректное имя. Имя должно начинаться с заглавной буквы и содержать только буквы.", file=sys.stderr)
     except KeyboardInterrupt:
         print("\nДо свидания!")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}", file=sys.stderr)
-
 
 def main():
     if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        greet_from_file(filename)
+        greet_from_file(sys.argv[1])
     else:
         greet_interactive()
-
 
 if __name__ == "__main__":
     main()

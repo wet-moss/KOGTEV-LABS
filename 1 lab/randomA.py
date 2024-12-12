2b99c1e26737c95201f06f5816fa94a4fc84efe5
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-12 23:55:02 +0300

    Update randomA.py

--- 1 lab/randomA.py
+++ 1 lab/randomA.py
@@ -4,14 +4,9 @@ import sys
 
 def main():
     try:
-        # Генерация случайного числа A
         A = random.randint(-10, 10)
-        
-        # Записываем значение A в logs.txt
         with open("logs.txt", "a") as log_file:
             log_file.write(f"A={A}\n")
-        
-        # Выводим значение A на консоль
         print(A)
     except Exception as e:
         print(f"Ошибка в программе randomA: {e}", file=sys.stderr)

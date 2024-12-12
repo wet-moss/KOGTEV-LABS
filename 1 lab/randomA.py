f2e3f1fa1982912443f459a31274db6d5123f7c9
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-12 23:51:29 +0300

    Update randomA.py

--- 1 lab/randomA.py
+++ 1 lab/randomA.py
@@ -4,23 +4,17 @@ import sys
 
 def main():
     try:
-        line = sys.stdin.readline().strip()
-        if not line:
-            raise ValueError("Нет данных для обработки")   
-        A = int(line)
-        B = random.randint(-10, 10)
+        # Генерация случайного числа A
+        A = random.randint(-10, 10)
+        
+        # Записываем значение A в logs.txt
         with open("logs.txt", "a") as log_file:
-            log_file.write(f"A={A}, B={B}\n")
-        if B == 0:
-            raise ZeroDivisionError("Попытка деления на ноль")
-        result = A / B
-        print(result)
-    except ValueError as ve:
-        print(f"Ошибка ввода: {ve}", file=sys.stderr)
-    except ZeroDivisionError as zde:
-        print(f"Ошибка: {zde}", file=sys.stderr)
+            log_file.write(f"A={A}\n")
+        
+        # Выводим значение A на консоль
+        print(A)
     except Exception as e:
-        print(f"Произошла непредвиденная ошибка: {e}", file=sys.stderr)
+        print(f"Ошибка в программе randomA: {e}", file=sys.stderr)
 
 if __name__ == "__main__":
     main()

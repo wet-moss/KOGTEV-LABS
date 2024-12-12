5aeefc5d39c6d3beb0bf5b971ea119c7e1655b38
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-12 23:48:04 +0300

    Update randomA.py

--- 1 lab/randomA.py
+++ 1 lab/randomA.py
@@ -4,10 +4,15 @@ import sys
 
 def main():
     try:
-        A = random.randint(-10, 10)
-        print(A)
+        A = int(input().strip())
+        B = random.randint(-10, 10)
+        if B == 0:
+            raise ValueError("Попытка деления на ноль")
+        result = A / B
+        print(result)
     except Exception as e:
-        print(f"Ошибка в программе randomA: {e}", file=sys.stderr)
+        print(f"Ошибка в программе ratio: {e}", file=sys.stderr)
         
 if __name__ == "__main__":
     main()
+

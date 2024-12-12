0a6bb091937cb38c2e2f1f9adff2ec4d61d57159
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-12 23:34:49 +0300

    Update ratio.py

--- 1 lab/ratio.py
+++ 1 lab/ratio.py
@@ -2,12 +2,16 @@
 import random
 import sys
 
-try:
-    A = int(input().strip())
-    B = random.randint(-10, 10)
-    if B == 0:
-        raise ValueError("Попытка деления на ноль")
-    result = A / B
-    print(result)
-except Exception as e:
-    print(f"Ошибка в программе ratio: {e}", file=sys.stderr)
+def main():
+    try:
+        A = int(input().strip())
+        B = random.randint(-10, 10)
+        if B == 0:
+            raise ValueError("Попытка деления на ноль")
+        result = A / B
+        print(result)
+    except Exception as e:
+        print(f"Ошибка в программе ratio: {e}", file=sys.stderr)
+        
+    if __name__ == "__main__":
+    main()

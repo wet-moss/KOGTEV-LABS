11ffafb2d25b4cffe0453a394215b5155e39d7ce
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-12 23:44:23 +0300

    Update squareRoot.py

--- 1 lab/squareRoot.py
+++ 1 lab/squareRoot.py
@@ -4,14 +4,17 @@ import math
 
 def main():
     try:
-        result = float(input().strip())
-        if result < 0:
-            raise ValueError("Необходимо ввести неотрицательное число")
-        sqrt_result = math.sqrt(result)
-        with open("output.txt", "w") as f:
-            f.write(str(sqrt_result))
+        num = float(input("Введите неотрицательное число: ").strip())
+        if num < 0:
+            raise ValueError("Необходимо ввести неотрицательное число")   
+        sqrt_result = math.sqrt(num) 
+        with open("output.txt", "a") as f:
+            f.write(f"{sqrt_result}\n")
+        
+    except ValueError as ve:
+        print(f"Ошибка ввода: {ve}", file=sys.stderr)
     except Exception as e:
-        print(f"Ошибка в программе squareRoot: {e}", file=sys.stderr)
+        print(f"Произошла ошибка: {e}", file=sys.stderr)
 
 if __name__ == "__main__":
     main()

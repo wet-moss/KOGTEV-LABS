47f3023d1b09e1d87a073cce046c818197d3a368
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-13 17:39:41 +0300

    Update ratio_rework.py

--- 1 lab/ratio_rework.py
+++ 1 lab/ratio_rework.py
@@ -11,15 +11,13 @@ def main():
         B = random.randint(-10, 10)
         with open("logs.txt", "a") as log_file:
             log_file.write(f"A={A}, B={B}\n")
-        if B == 0:
-            raise ZeroDivisionError("Попытка деления на ноль")
         result = A / B
         print(result)
 
     except ValueError as ve:
         print(f"Ошибка ввода: {ve}", file=sys.stderr)
-    except ZeroDivisionError as zde:
-        print(f"Ошибка: {zde}", file=sys.stderr)
+    except ZeroDivisionError:
+        print(f"Ошибка (деление на ноль)", file=sys.stderr)
     except Exception as e:
         print(f"Произошла ошибка: {e}", file=sys.stderr)
 

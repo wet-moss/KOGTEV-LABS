b02627d41bd27e3b91392f12745d8f2c543ae284
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-15 20:03:09 +0300

    Update randomA_rework.py

--- 1 lab/randomA_rework.py
+++ 1 lab/randomA_rework.py
@@ -6,8 +6,6 @@ def main():
     print(A)
     with open("logs.txt", "a") as log_file:
         log_file.write(f"A = {A}\n")
-    with open("output.txt", "a") as output_file:
-        output_file.write(f"{A}\n")
 
 if __name__ == "__main__":
     main()

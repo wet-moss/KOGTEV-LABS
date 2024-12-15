be93f41de40f3c4607cf13aa25df24fb2d137f64
wet-moss <157098408+wet-moss@users.noreply.github.com>
2024-12-15 19:58:14 +0300

    Update randomA_rework.py

--- 1 lab/randomA_rework.py
+++ 1 lab/randomA_rework.py
@@ -4,8 +4,10 @@ import random
 def main():
     A = random.randint(-10, 10)
     print(A)
+    with open("logs.txt", "a") as log_file:
+        log_file.write(f"A = {A}\n")
     with open("output.txt", "a") as output_file:
-        output_file.write(f"A = {A}\n")
+        output_file.write(f"{A}\n")
 
 if __name__ == "__main__":
     main()
